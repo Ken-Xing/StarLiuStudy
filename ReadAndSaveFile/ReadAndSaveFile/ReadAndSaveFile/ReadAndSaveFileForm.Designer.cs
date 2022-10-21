c8d58661d977aaaf2ef0c127b24508ff9481df5f
Starliu163 <star.liu@itlogica.com>
2022-10-21 09:41:43 +0800

    v1.0
    只完成了最基本的读取和保存文件内容到数据库的最基本的功能

---
+++ ReadAndSaveFile/ReadAndSaveFile/ReadAndSaveFile/ReadAndSaveFileForm.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace ReadAndSaveFile
+{
+    partial class ReadAndSaveFileForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnReadFile = new System.Windows.Forms.Button();
+            this.dgvDataTable = new System.Windows.Forms.DataGridView();
+            this.btnSaveFile = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDataTable)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnReadFile
+            // 
+            this.btnReadFile.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btnReadFile.AutoEllipsis = true;
+            this.btnReadFile.Location = new System.Drawing.Point(299, 415);
+            this.btnReadFile.Name = "btnReadFile";
+            this.btnReadFile.Size = new System.Drawing.Size(75, 23);
+            this.btnReadFile.TabIndex = 2;
+            this.btnReadFile.Text = "Open file";
+            this.btnReadFile.UseVisualStyleBackColor = true;
+            this.btnReadFile.Click += new System.EventHandler(this.btnReadFile_Click);
+            // 
+            // dgvDataTable
+            // 
+            this.dgvDataTable.AllowUserToAddRows = false;
+            this.dgvDataTable.AllowUserToDeleteRows = false;
+            this.dgvDataTable.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvDataTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDataTable.Location = new System.Drawing.Point(0, 0);
+            this.dgvDataTable.Name = "dgvDataTable";
+            this.dgvDataTable.ReadOnly = true;
+            this.dgvDataTable.RowHeadersWidth = 51;
+            this.dgvDataTable.RowTemplate.Height = 23;
+            this.dgvDataTable.Size = new System.Drawing.Size(799, 393);
+            this.dgvDataTable.TabIndex = 0;
+            // 
+            // btnSaveFile
+            // 
+            this.btnSaveFile.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btnSaveFile.Location = new System.Drawing.Point(380, 416);
+            this.btnSaveFile.Name = "btnSaveFile";
+            this.btnSaveFile.Size = new System.Drawing.Size(75, 23);
+            this.btnSaveFile.TabIndex = 3;
+            this.btnSaveFile.Text = "Save file";
+            this.btnSaveFile.UseVisualStyleBackColor = true;
+            this.btnSaveFile.Click += new System.EventHandler(this.btnSaveFile_Click);
+            // 
+            // ReadAndSaveFileForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSaveFile);
+            this.Controls.Add(this.btnReadFile);
+            this.Controls.Add(this.dgvDataTable);
+            this.Name = "ReadAndSaveFileForm";
+            this.Text = "ReadAndSaveFileForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDataTable)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnReadFile;
+        private System.Windows.Forms.DataGridView dgvDataTable;
+        private System.Windows.Forms.Button btnSaveFile;
+    }
+}
---
+++ ReadAndSaveFile/ReadAndSaveFile/ReadAndSaveFile/ReadAndSaveFileForm.cs
@@ -0,0 +1,186 @@
+﻿using ReadAndSaveCSVFile;
+using System;
+using System.Data;
+using System.IO;
+using System.Windows.Forms;
+namespace ReadAndSaveFile
+{
+    public partial class ReadAndSaveFileForm : Form
+    {
+        #region member
+        private string _filePath = string.Empty;
+        private StreamReader _streamReader = null;
+        private string _connStr = string.Empty;
+        private CSVFileHelper _cSVFileHelper = new CSVFileHelper();
+        #endregion
+
+
+        public ReadAndSaveFileForm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Reads the file and displays its contents
+        /// </summary>
+        /// <param name="sender">The btnReadFile_Click object itself</param>
+        /// <param name="e">Record additional information for clicking btnReadFile</param>
+        private void btnReadFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                //Get file Path
+                this._filePath = openFileDialog.FileName;
+
+                if (this._filePath != string.Empty && Path.GetExtension(this._filePath) == ".csv")
+                {
+                    try
+                    {
+                        this._streamReader = new StreamReader(this._filePath);
+                        //Get file content
+                        this._cSVFileHelper.GetFileDataToDataTable(this._streamReader);
+                        //Display csv file datatable
+                        this.dgvDataTable.DataSource = this._cSVFileHelper.CsvContentDataTable.Copy();
+                    }
+                    catch (IOException)
+                    {
+                        throw new ReadAndSaveFileException("The file is occupied by another program");
+                    }
+                    catch (ReadAndSaveFileException ex)
+                    {
+                        MessageBox.Show(ex.ExceptionMessage.ToString());
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Please select the extension '.csv' file.");
+                }
+            }
+            else
+            {
+
+                if (this._streamReader != null)
+                {
+                    this._streamReader.Close();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Save file content data 
+        /// </summary>
+        /// <param name="sender">The btnSaveFile_Click object itself</param>
+        /// <param name="e">Record additional information for clicking btnSaveFile</param>
+        private void btnSaveFile_Click(object sender, EventArgs e)
+        {
+            this._connStr = "Server = 192.168.0.236,1433; Initial Catalog = ITLTest;user Id = EKSDBUser; Password = qwe123!@#;";
+            DbBase.DbHelper dbhelper = new DbBase.DbHelper(this._connStr);
+            string fileContent = string.Empty;
+            int nameMaxLength = 64;
+            int maximumAge = 35;
+            int minimumAge = 10;
+            int maximumSNumber = 202299999;
+            int minimumSNumber = 202200000;
+            int emailMaxLength = 64;
+            DateTime minimumDate = new DateTime(2022, 1, 3);
+            DateTime maxminumDate = new DateTime(2022, 12, 25);
+            string targetTable = "StudentAdmissionInfo";
+            string findDataSql = "EXEC sp_CheckStudentAdmissionInfoSnumberIsExists @Snumber = @Snumber";
+            DataTable dublicateDataTable = new DataTable();
+            DataTable notDublicateDataTable = new DataTable();
+            string updateDataSql = "EXEC sp_UpdateStudentAdmissionInfoBySnmber @Name = @Name, @Age = @Age, @Snumber = @Snumber, @Sex = @Sex, @Email = @Email, @AdmissionDate = @AdmissionDate";
+            ReadAndSaveFileDbHelper readAndSaveFileDbHelper = new ReadAndSaveFileDbHelper(this._connStr);
+
+            if (this._cSVFileHelper.CsvContentDataTable != null)
+            {
+                //Get the file content data
+                try
+                {
+                    //Determines if it matches the structure of the data table in the database
+                    if (this._cSVFileHelper.CheckIsMatch(nameMaxLength, maximumAge, minimumAge, maximumSNumber, minimumSNumber, emailMaxLength, minimumDate, maxminumDate))
+                    {
+                        //Check if the file contains duplicate content
+                        if (this._cSVFileHelper.CheckCSVDataIsRepeated())
+                        {
+                            //Get duplicate and non-duplicate data
+                            this._cSVFileHelper.GetReapeatedAndNotRepeatedData(findDataSql, this._connStr);
+                            notDublicateDataTable = this._cSVFileHelper.NotDuplicateDataTable.Copy();
+                            dublicateDataTable = this._cSVFileHelper.DuplicateDataTable.Copy();
+
+                            if (dublicateDataTable.Rows.Count > 0)
+                            {
+                                DialogResult dialogResult = MessageBox.Show("The data in the file and the data in the database are duplicated.Do you override duplicate data?", "File content duplication prompts", MessageBoxButtons.YesNo);
+                                //Duplicate data is overwritten if it exists
+                                if (dialogResult.Equals(DialogResult.Yes))
+                                {
+                                    //Insert non-duplicate data and update duplicate data
+                                    if (readAndSaveFileDbHelper.SaveAndUpdateBulkData(updateDataSql, dublicateDataTable, notDublicateDataTable, targetTable))
+                                    {
+                                        this._filePath = string.Empty;
+                                        this._cSVFileHelper.DestroyCSVContentDataTable();
+                                        this.dgvDataTable.DataSource = null;
+                                        this._streamReader.Close();
+                                        MessageBox.Show("Success");
+                                    }
+                                    else
+                                    {
+                                        MessageBox.Show("Fail");
+                                    }
+
+                                }
+                                //Insert data that is not duplicated
+                                else
+                                {
+                                    //save file content data to database
+                                    if (dbhelper.SaveBulkNotDuplicateData(notDublicateDataTable, targetTable))
+                                    {
+                                        this._filePath = string.Empty;
+                                        this._cSVFileHelper.DestroyCSVContentDataTable();
+                                        this.dgvDataTable.DataSource = null;
+                                        this._streamReader.Close();
+                                        MessageBox.Show("Success");
+                                    }
+                                    else
+                                    {
+                                        MessageBox.Show("Fail");
+                                    }
+                                }
+                            }
+                            //Holds the contents of data that is not duplicated
+                            else
+                            {
+                                if (dbhelper.SaveBulkNotDuplicateData(notDublicateDataTable, targetTable))
+                                {
+                                    this._filePath = string.Empty;
+                                    this._cSVFileHelper.DestroyCSVContentDataTable();
+                                    this.dgvDataTable.DataSource = null;
+                                    this._streamReader.Close();
+                                    MessageBox.Show("Success");
+                                }
+                                else
+                                {
+                                    MessageBox.Show("Fail");
+                                }
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("");
+                        }
+                    }
+                }
+                //Catch custom exceptions
+                catch (ReadAndSaveFileException ex)
+                {
+                    MessageBox.Show(ex.ExceptionMessage.ToString());
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select the file you want to open!");
+            }
+        }
+    }
+}
